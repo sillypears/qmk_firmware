ed88d4cb9d7f703e67e49e9e0fd72441f84388c0
sillypears <sillypears@users.noreply.github.com>
2020-03-20 06:51:21 -0400

    Daisy things

--- keyboards/daisy/keymaps/pears/keymap.c
+++ keyboards/daisy/keymaps/pears/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     GRAVE_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,     KC_U,   KC_I,    KC_O,    KC_P,    KC_SLASH,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,     KC_J,   KC_K,    KC_L,    KC_ENT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,     KC_M,   KC_COMM, KC_DOT,  KC_RGUI,
-    KC_TRNS, KC_LCTL, KC_LALT, KC_BSPC,  KC_SPC, LOWER, RAISE, KC_TRNS
+    XXXXXXX, KC_LCTL, KC_LALT, KC_BSPC,  KC_SPC, LOWER, RAISE, XXXXXXX
   ),
 
   /* Function Layer
@@ -44,9 +44,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LW] = LAYOUT(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
-    KC_TRNS, KC_TRNS,  KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS, KC_EQL, KC_TRNS,
-    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_SCLN, KC_QUOT, KC_TRNS,
-    KC_TRNS, KC_LGUI, KC_LALT, KC_DEL,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS
+    _______, XXXXXXX,  KC_UP,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MINS, KC_EQL, _______,
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, KC_SCLN, KC_QUOT, _______,
+    XXXXXXX, _______, _______, KC_DEL,  _______,   _______, _______, XXXXXXX
   ),
 
   /* Second Function Layer
@@ -61,10 +61,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *         '-------------------------------------------------------'
    */
   [_RS] = LAYOUT(
-    RESET,  KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_TRNS, KC_HOME, KC_PGUP,  KC_TRNS,  KC_PSCR,
-    KC_TRNS, KC_F7,   KC_F8,    KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_END,  KC_PGDN,  KC_TRNS,
-    KC_TRNS, RGB_TOG, RGB_MOD,  RGB_HUD, RGB_HUI, RGB_VAD, RGB_VAI, RGB_SAD, RGB_SAI, BL_STEP,  KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    RESET,  KC_F1,   KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,   XXXXXXX, KC_HOME, KC_PGUP,  XXXXXXX,  KC_PSCR,
+    _______, KC_F7,   KC_F8,    KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX, KC_END,  KC_PGDN,  _______,
+    _______, RGB_TOG, RGB_MOD,  RGB_HUD, RGB_HUI, RGB_VAD, RGB_VAI, RGB_SAD, RGB_SAI, BL_STEP,  _______,
+    XXXXXXX, _______, _______,  KC_BSPC, _______, _______, _______, XXXXXXX
   ),
 
 };
