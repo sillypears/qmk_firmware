d795bbd91009487f1595039bd3bbfa2443022c58
sillypears <sillypears@users.noreply.github.com>
2020-04-02 20:11:57 -0400

    a bunch of shit i don't know

--- keyboards/kbdfans/kbd75/keymaps/pears/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/pears/keymap.c
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGUP,
     KC_LCTL,   KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                      KC_ENT,   KC_PGDN,
     KC_LSFT,  KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_END,
-    KC_LGUI,  KC_LALT,  KC_LALT,                      KC_SPC,   KC_SPC,   KC_SPC,                       KC_RGUI,  KC_RALT,  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
+    KC_LGUI,  KC_LALT,  XXXXXXX,                      XXXXXXX,   KC_SPC,   XXXXXXX,                       KC_RGUI,  KC_RALT,  KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
 
   [_FL] = LAYOUT(
---
+++ keyboards/tkc1800/keymaps/pears/rules.mk
@@ -0,0 +1,7 @@
+VIA_ENABLE = yes
+
+BOOTMAGIC_ENABLE = lite
+BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality on B7 by default
+RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight. Do not enable this with audio at the same time.
+
+MOUSEKEY_ENABLE  = no
