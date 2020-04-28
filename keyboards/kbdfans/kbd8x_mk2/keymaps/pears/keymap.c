ce7109a0d02e40aa0c8f73d7845af2a686331864
sillypears <sillypears@users.noreply.github.com>
2020-04-28 14:25:27 -0400

    mk2 updates

--- keyboards/kbdfans/kbd8x_mk2/keymaps/pears/keymap.c
+++ keyboards/kbdfans/kbd8x_mk2/keymaps/pears/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                   KC_ENT,                                      \
 	KC_LSFT, KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_DEL,                    KC_UP,            \
 	KC_LCTL, KC_LGUI, KC_LALT,                           KC_SPC,                                       KC_RALT, KC_RGUI, MO(_SL),  KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT  \
-  ),,
+  ),
   [_SL] = LAYOUT( /* Second */
 
 	XXXXXXX,          RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                  _______, _______, _______, \
